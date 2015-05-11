834f52c973ff809ebf734251b27f786158bc98d4
Darren Mason <djmason9@gmail.com>
2015-05-11 09:07:42 -0700

    added widget

---
+++ yo2Widget/TodayViewController.h
@@ -0,0 +1,15 @@
+//
+//  TodayViewController.h
+//  yo2Widget
+//
+//  Created by Darren Mason on 5/10/15.
+//  Copyright (c) 2015 Mason, Darren J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+
+
+@end
---
+++ yo2Widget/TodayViewController.m
@@ -0,0 +1,46 @@
+//
+//  TodayViewController.m
+//  yo2Widget
+//
+//  Created by Darren Mason on 5/10/15.
+//  Copyright (c) 2015 Mason, Darren J. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@property (strong, nonatomic) NSMutableArray *messageArray;
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.preferredContentSize = CGSizeMake(0, 150);
+    NSString *path = [[NSBundle mainBundle] pathForResource: @"messages" ofType:@"plist"];
+    
+    // Build the array from the messages plist
+    _messageArray = [[NSMutableArray alloc] initWithContentsOfFile:path];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+
+@end
